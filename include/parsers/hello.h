5736851cf751d74d1b3965f18c62309181fe9318
Juan <jdeluca@itba.edu.ar>
2022-06-01 23:32:28 -0300

    Auto stash before merge of "parser" and "origin/parser"

--- include/parsers/hello.h
+++ include/parsers/hello.h
@@ -11,24 +11,26 @@
 
 #define VERSION_FIELD = 0x05;
 
-typedef enum hello_state {
+typedef enum hello_state
+{
   VERSION = 0,
   NAUTH,
   AUTH,
   DONE,
   ERROR_INV_VERSION, // ?
-  ERROR_INV_AUTH, // ?
+  ERROR_INV_AUTH,    // ?
   ERROR_UNASSIGNED_METHOD,
   ERROR_UNSUPPORTED_METHOD
 } hello_state;
 
-struct hello_parser {
+struct hello_parser
+{
   uint8_t version;
   // Ah son lo mismo estos dos, ver con cual quedarse, no entiendo lo de *
-  uint8_t NMETHODS;  
+  uint8_t NMETHODS;
   uint8_t METHODS;
   uint8_t non_auth; // ?
-  uint8_t * auth;
+  uint8_t *auth;
 
   unsigned int bytes_to_read;
   hello_state state;
@@ -40,21 +42,34 @@ enum hello_methods {
   NO_ACCEPTABLE = 0xFF
 }
 
-void hello_parser_init (hello_parser p);
+/*
+static const uint8_t SOCKS_HELLO_NOAUTHENTICATION_REQUIRED = 0x00;
 
-enum hello_state hello_read_next (hello_parser p, const uint8_t b);
-enum hello_state hello_consume (buffer * b, hello_parser p, bool *errored);
+/** Constant for user password auth */
+static const uint8_t SOCKS_HELLO_USERPASS = 0x02;
+/*
+ * If the selected METHOD is X'FF', none of the methods listed by the
+   client are acceptable, and the client MUST close the connection.
+ */
+static const uint8_t SOCKS_HELLO_NO_ACCEPTABLE_METHODS = 0xFF;
+* /
 
 int parsing_done (hello_parser p, bool *errored); // podria ser bool o no
 int is_done (hello_state s, bool *errored);
 
-uint8_t get_nauth (hello_parser p);
-const uint8_t * get_auth_types (hello_parser p);
+enum hello_state hello_read_next(hello_parser p, const uint8_t b);
+enum hello_state hello_consume(buffer *b, hello_parser p, bool *errored);
 
-int hello_marshall (buffer *b, const uint8_t method);
+int parsing_done(hello_parser p, bool *errored);
+int is_done(hello_state s, bool *errored);
 
-void free (hello_parser p);
+uint8_t get_nauth(hello_parser p);
+const uint8_t *get_auth_types(hello_parser p);
 
-hello_state get_state (hello_parser p);
+int hello_marshall(buffer *b, const uint8_t method);
+
+void free(hello_parser p);
+
+hello_state get_state(hello_parser p);
 
 #endif
