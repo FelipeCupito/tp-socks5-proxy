6ee38f79197676fb3bce11cb3d5176e60bca5536
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-06-02 18:07:15 -0300

    request.c done

--- src/parsers/request.c
+++ src/parsers/request.c
@@ -61,9 +61,11 @@ static enum request_state atyp (const uint8_t c, struct request_parser* p) {
   return next;
 }
 
-static enum request_state dst_addr_fqdn(const uint8_t c, struct request_parser* p) {
+static enum request_state dstaddr_fqdn(const uint8_t c, struct request_parser* p) {
+  // el byte nos dice cuanto nos queda por leer
   remaining_set(p,c);
   // TODO este puede llegar a ser bytes_read
+  // TODO no entendi que hace
   p -> request -> dst_addr.fqdn[p->total_bytes_to_read - 1] = 0; 
 
   return request_dstaddr;
@@ -72,9 +74,95 @@ static enum request_state dst_addr_fqdn(const uint8_t c, struct request_parser* 
 static enum request_state dstaddr(const uint8_t c, struct request_parser* p) {
   enum request_state next;
 
+  // TODO no entiendo esto
+  // TODO completar
   switch (p -> request -> dst_addr_type) {
     case socks_req_addrtype_ipv4:
-      ((uint8_t *)&(p -> request -> dst_addr.ipv4.sin_addr))[]
+      // TODO completar
+      // Esta linea se ve cortada en la clase
+      ((uint8_t *)&(p -> request -> dst_addr.ipv4.sin_addr))[p -> bytes_read++] = c;
+      break;
+    case socks_req_addrtype_ipv6:
+      ((uint8_t *)&(p -> request -> dst_addr.ipv6.sin6_addr))[p -> bytes_read++] = c;
+      break;
+    case socks_req_addrtype_domain:
+      p -> request -> dst_addr.fqdn[p -> i++] = c;
       break;
   }
+
+  if (remaining_is_done(p)) {
+    remaining_set(p,2);
+    p -> request -> dst_port = 0;
+    next = request_dstport;
+  } else {
+    next = request_dstaddr;
+  }
+
+  return next;
+}
+
+static enum request_state dstport(const uint8_t c, struct request_parser* p) {
+  enum request_state next;
+  // TODO no entiendo que es esto
+  *(((uint8_t *) &(p -> request -> dst_port)) + p -> bytes_read) = c;
+  p -> bytes_read++;
+  if ( p -> bytes_read >= p -> total_bytes_to_read) {
+    next = request_done;
+  } else {
+    next = request_dstport;
+  }
+
+  return next;
+}
+
+extern void request_parser_init (struct reques_parser* p) {
+  p -> state = request_version;
+  memset(p -> request, 0, sizeof(*(p -> request)));
+}
+
+extern enum request_state request_state_feed (struct request_parser* p, const uint8_t c) {
+  enum request_state next;
+
+  switch (p -> state) {
+    case request_version:
+  }
+}
+
+extern enum request_state reques_parser_feed (struct request_parser* p, const uint8_t c) {
+  enum request_state next;
+
+  switch (p -> state) {
+    case request_version:
+      next = version(c,p);
+      break;
+    case request_cmd:
+      next = cmd(c,p);
+      break;
+    case request_rsv:
+      next = rsv(c,p);
+      break;
+    case request_atyp:
+      next = atyp(c,p);
+      break;
+    case request_dstaddr_fqdn:
+      next = dstaddr_fqdn(c,p);
+      break;
+    case request_dstaddr:
+      next = dst_addr(c,p);
+      break;
+    case request_dstport:
+      next = dst_port(c,p);
+      break;
+    case request_done:
+    case request_error:
+    case reques_error_unsupported_version:
+    case request_error_unsupported_addresstype:
+      next = p -> state;
+      break;
+    default:
+      next = request_error;
+      break;
+  }
+
+  return next;
 }
