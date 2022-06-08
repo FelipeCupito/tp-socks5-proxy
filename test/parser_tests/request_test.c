befec312cb983db0d3303a2997ab606b11d90a2e
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-06-08 15:26:39 -0300

    invalid cmd test done and passing

--- test/parser_tests/request_test.c
+++ test/parser_tests/request_test.c
@@ -37,84 +37,6 @@ START_TEST(test_request_connect_ipv4) {
 }
 END_TEST
 
-START_TEST(test_request_connect_fqdn) {
-    struct request req; 
-    struct request_parser parser = {
-        .request = &req
-    };
-    request_parser_init(&parser);
-
-    uint8_t data[] = {
-        0x05, // version
-        0x01, // connect
-        0x00, // reserved
-        0x03, // atyp: fqdn
-        0x0E, // len
-        0x77, 0x77, 0x77, 0x2E,              // www.
-        0x67, 0x6F, 0x6F, 0x67, 0x6C, 0x65, // google
-        0x2E, 0x63, 0x6F, 0x6D,             // .com
-        0x23, 0x82, // dst port: 9090
-    };
-
-    buffer b;
-    FIXBUF(b, data);
-    bool errored = false;
-    enum request_state st = request_consume(&b, &parser, &errored);
-    ck_assert_uint_eq(false, errored);
-    ck_assert_uint_eq(socks_req_cmd_connect, req.cmd);
-    ck_assert_uint_eq(socks_req_addrtype_domain, req.dst_addr_type);
-    ck_assert_str_eq("www.google.com", req.dst_addr.fqdn);
-    ck_assert_uint_eq(htons(9090), req.dst_port);
-    ck_assert_uint_eq(request_done, st);
-}
-END_TEST
-
-START_TEST(test_request_connect_invalid_atyp) {
-    struct request req; 
-    struct request_parser parser = {
-        .request = &req
-    };
-    request_parser_init(&parser);
-    uint8_t data[] = {
-        0x05, // version
-        0x01, // connect
-        0x00, // reserved
-        0x0F, // atyp: invalid
-        0x7F, 0x00, 0x00, 0x01, // dst addr: 127.0.0.1
-        0x23, 0x82, // dst port: 9090
-    };
-    buffer b;
-    FIXBUF(b, data);
-    bool errored = false;
-    enum request_state st = request_consume(&b, &parser, &errored);
-    ck_assert_uint_eq(true, errored);
-    ck_assert_uint_eq(request_error_unsupported_addresstype, st);
-}
-END_TEST
-
-START_TEST(test_request_connect_invalid_version) {
-    struct request req; 
-    struct request_parser parser = {
-        .request = &req
-    };
-    request_parser_init(&parser);
-    uint8_t data[] = {
-        0x03,                   // version
-        0x01,                   // connect
-        0x00,                   // reserved
-        0x01,                   // atyp: ipv4
-        0x7F, 0x00, 0x00, 0x01, // dst addr: 127.0.0.1
-        0x23, 0x82, // dst port: 9090
-    };
-    buffer b;
-    FIXBUF(b, data);
-    bool errored = false;
-    enum request_state st = request_consume(&b, &parser, &errored);
-    ck_assert_uint_eq(true, errored);
-    ck_assert_uint_eq(request_error_unsupported_version, st);
-}
-END_TEST
-
 START_TEST(test_request_connect_ipv6) {
     struct request req; 
     struct request_parser parser = {
@@ -144,6 +66,107 @@ START_TEST(test_request_connect_ipv6) {
 }
 END_TEST
 
+START_TEST(test_request_connect_fqdn) {
+    struct request req; 
+    struct request_parser parser = {
+        .request = &req
+    };
+    request_parser_init(&parser);
+
+    uint8_t data[] = {
+        0x05,                               // version
+        0x01,                               // connect
+        0x00,                               // reserved
+        0x03,                               // atyp: fqdn
+        0x0E,                               // len
+        0x77, 0x77, 0x77, 0x2E,             // www.
+        0x67, 0x6F, 0x6F, 0x67, 0x6C, 0x65, // google
+        0x2E, 0x63, 0x6F, 0x6D,             // .com
+        0x23, 0x82,                         // dst port: 9090
+    };
+
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum request_state st = request_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(false, errored);
+    ck_assert_uint_eq(socks_req_cmd_connect, req.cmd);
+    ck_assert_uint_eq(socks_req_addrtype_domain, req.dst_addr_type);
+    ck_assert_str_eq("www.google.com", req.dst_addr.fqdn);
+    ck_assert_uint_eq(htons(9090), req.dst_port);
+    ck_assert_uint_eq(request_done, st);
+}
+END_TEST
+
+START_TEST(test_request_connect_invalid_atyp) {
+    struct request req; 
+    struct request_parser parser = {
+        .request = &req
+    };
+    request_parser_init(&parser);
+    uint8_t data[] = {
+        0x05,                   // version
+        0x01,                   // connect
+        0x00,                   // reserved
+        0x0F,                   // atyp: invalid
+        0x7F, 0x00, 0x00, 0x01, // dst addr: 127.0.0.1
+        0x23, 0x82,             // dst port: 9090
+    };
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum request_state st = request_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(true, errored);
+    ck_assert_uint_eq(request_error_unsupported_addresstype, st);
+}
+END_TEST
+
+START_TEST(test_request_connect_invalid_version) {
+    struct request req; 
+    struct request_parser parser = {
+        .request = &req
+    };
+    request_parser_init(&parser);
+    uint8_t data[] = {
+        0x03,                   // version
+        0x01,                   // connect
+        0x00,                   // reserved
+        0x01,                   // atyp: ipv4
+        0x7F, 0x00, 0x00, 0x01, // dst addr: 127.0.0.1
+        0x23, 0x82,             // dst port: 9090
+    };
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum request_state st = request_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(true, errored);
+    ck_assert_uint_eq(request_error_unsupported_version, st);
+}
+END_TEST
+
+START_TEST(test_request_connect_invalid_cmd) {
+    struct request req; 
+    struct request_parser parser = {
+        .request = &req
+    };
+    request_parser_init(&parser);
+    uint8_t data[] = {
+        0x05,                   // version
+        0xF1,                   // invalid
+        0x00,                   // reserved
+        0x01,                   // atyp: ipv4
+        0x7F, 0x00, 0x00, 0x01, // dst addr: 127.0.0.1
+        0x23, 0x82,             // dst port: 9090
+    };
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum request_state st = request_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(true, errored);
+    ck_assert_uint_eq(request_error_unsupported_cmd, st);
+}
+END_TEST
+
 Suite*
 suite(void) {
     Suite* s = suite_create("request");
@@ -151,6 +174,14 @@ suite(void) {
     TCase* testcase_request_connect_ipv4 = tcase_create("request_connect_ipv4");
     tcase_add_test(testcase_request_connect_ipv4, test_request_connect_ipv4);
     suite_add_tcase(s, testcase_request_connect_ipv4);
+
+    TCase* testcase_request_connect_ipv6 = tcase_create("request_connect_ipv6");
+    tcase_add_test(testcase_request_connect_ipv6, test_request_connect_ipv6);
+    suite_add_tcase(s, testcase_request_connect_ipv6);
+
+    TCase* testcase_request_connect_fqdn = tcase_create("request_connect_fqdn");
+    tcase_add_test(testcase_request_connect_fqdn, test_request_connect_fqdn);
+    suite_add_tcase(s, testcase_request_connect_fqdn);
 
     TCase* testcase_request_connect_invalid_atyp = tcase_create("request_connect_invalid_atyp");
     tcase_add_test(testcase_request_connect_invalid_atyp, test_request_connect_invalid_atyp);
@@ -160,13 +191,9 @@ suite(void) {
     tcase_add_test(testcase_request_connect_invalid_version, test_request_connect_invalid_version);
     suite_add_tcase(s, testcase_request_connect_invalid_version);
 
-    TCase* testcase_request_connect_ipv6 = tcase_create("request_connect_ipv6");
-    tcase_add_test(testcase_request_connect_ipv6, test_request_connect_ipv6);
-    suite_add_tcase(s, testcase_request_connect_ipv6);
-
-    TCase* testcase_request_connect_fqdn = tcase_create("request_connect_fqdn");
-    tcase_add_test(testcase_request_connect_fqdn, test_request_connect_fqdn);
-    suite_add_tcase(s, testcase_request_connect_fqdn);
+    TCase* testcase_request_connect_invalid_cmd = tcase_create("request_connect_invalid_cmd");
+    tcase_add_test(testcase_request_connect_invalid_cmd, test_request_connect_invalid_cmd);
+    suite_add_tcase(s, testcase_request_connect_invalid_cmd);
 
     return s;
 }
