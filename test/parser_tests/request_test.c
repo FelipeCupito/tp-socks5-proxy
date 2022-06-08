1d0c35b08b91304e8d365f1d24d6ce4a6d9b9631
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-06-08 13:43:02 -0300

    test case unsupp version done

--- test/parser_tests/request_test.c
+++ test/parser_tests/request_test.c
@@ -60,6 +60,29 @@ START_TEST(test_request_connect_invalid_atyp) {
 }
 END_TEST
 
+START_TEST(test_request_connect_invalid_version) {
+    struct request req; 
+    struct request_parser parser = {
+        .request = &req
+    };
+    request_parser_init(&parser);
+    uint8_t data[] = {
+        0x03, // version
+        0x01, // connect
+        0x00, // reserved
+        0x01, // atyp: invalid
+        0x7F, 0x00, 0x00, 0x01, // dst addr: 127.0.0.1
+        0x23, 0x82, // dst port: 9090
+    };
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum request_state st = request_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(true, errored);
+    ck_assert_uint_eq(request_error_unsupported_version, st);
+}
+END_TEST
+
 START_TEST(test_request_connect_ipv6) {
     struct request req; 
     struct request_parser parser = {
@@ -105,6 +128,10 @@ suite(void) {
     TCase* testcase_request_connect_invalid_atyp = tcase_create("request_connect_invalid_atyp");
     tcase_add_test(testcase_request_connect_invalid_atyp, test_request_connect_invalid_atyp);
     suite_add_tcase(s, testcase_request_connect_invalid_atyp);
+
+    TCase* testcase_request_connect_invalid_version = tcase_create("request_connect_invalid_version");
+    tcase_add_test(testcase_request_connect_invalid_version, test_request_connect_invalid_version);
+    suite_add_tcase(s, testcase_request_connect_invalid_version);
 
     TCase* testcase_request_connect_ipv6 = tcase_create("request_connect_ipv6");
     tcase_add_test(testcase_request_connect_ipv6, test_request_connect_ipv6);
