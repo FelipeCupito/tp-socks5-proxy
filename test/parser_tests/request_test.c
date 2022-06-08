448de29f344c10873286cf2c465506c609d0c109
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-06-08 12:28:16 -0300

    invalid address type test added and passing

--- test/parser_tests/request_test.c
+++ test/parser_tests/request_test.c
@@ -37,6 +37,32 @@ START_TEST(test_request_connect_ipv4) {
 }
 END_TEST
 
+START_TEST(test_request_connect_invalid_atyp) {
+    struct request req; 
+    struct request_parser parser = {
+        .request = &req
+    };
+    request_parser_init(&parser);
+    uint8_t data[] = {
+        0x05, // version
+        0x01, // connect
+        0x00, // reserved
+        0x0F, // atyp: ipv4
+        0x7F, 0x00, 0x00, 0x01, // dst addr: 127.0.0.1
+        0x23, 0x82, // dst port: 9090
+    };
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum request_state st = request_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(true, errored);
+    // ck_assert_uint_eq(socks_req_cmd_connect, req.cmd);
+    // ck_assert_uint_eq(socks_req_addrtype_ipv4, req.dst_addr_type);
+    // ck_assert_uint_eq(htons(9090), req.dst_port);
+    ck_assert_uint_eq(request_error_unsupported_addresstype, st);
+}
+END_TEST
+
 START_TEST(test_request_connect_ipv6) {
     struct request req; 
     struct request_parser parser = {
@@ -78,6 +104,10 @@ suite(void) {
     TCase* testcase_request_connect_ipv4 = tcase_create("request_connect_ipv4");
     tcase_add_test(testcase_request_connect_ipv4, test_request_connect_ipv4);
     suite_add_tcase(s, testcase_request_connect_ipv4);
+
+    TCase* testcase_request_connect_invalid_atyp = tcase_create("request_connect_invalid_atyp");
+    tcase_add_test(testcase_request_connect_invalid_atyp, test_request_connect_invalid_atyp);
+    suite_add_tcase(s, testcase_request_connect_invalid_atyp);
 
     TCase* testcase_request_connect_ipv6 = tcase_create("request_connect_ipv6");
     tcase_add_test(testcase_request_connect_ipv6, test_request_connect_ipv6);
