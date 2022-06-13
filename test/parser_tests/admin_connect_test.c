8020949a2fd0a9d6a17c97e51c95e7ee1e31d7df
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2022-06-13 11:55:27 -0300

    admin connect test pass

--- test/parser_tests/admin_connect_test.c
+++ test/parser_tests/admin_connect_test.c
@@ -24,8 +24,45 @@ START_TEST(test_admin_connect_normal)
     bool errored = false;
     enum admin_connect_state st = admin_connect_consume(&b, &parser, &errored);
     ck_assert_uint_eq(false, errored);
+		ck_assert_uint_eq(0, parser.version);
     ck_assert_str_eq("FOO", (char*) parser.password.passwd);
     ck_assert_uint_eq(admin_connect_done, st);
+}
+END_TEST
+
+START_TEST(test_admin_connect_unsupported_version)
+{
+    struct admin_connect_parser parser;
+    admin_connect_parser_init(&parser);
+    uint8_t data[] = {
+        0xFF, // version
+        0x03, // pass len
+        0x46, 0x4F, 0x4F, // foo
+    };
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum admin_connect_state st = admin_connect_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(true, errored);
+    ck_assert_uint_eq(admin_connect_error_version, st);
+}
+END_TEST
+
+START_TEST(test_admin_connect_passlen_zero)
+{
+    struct admin_connect_parser parser;
+    admin_connect_parser_init(&parser);
+    uint8_t data[] = {
+        0x00, // version
+        0x00, // pass len
+        0x46, 0x4F, 0x4F, // foo
+    };
+    buffer b;
+    FIXBUF(b, data);
+    bool errored = false;
+    enum admin_connect_state st = admin_connect_consume(&b, &parser, &errored);
+    ck_assert_uint_eq(true, errored);
+    ck_assert_uint_eq(admin_connect_error_passlen, st);
 }
 END_TEST
 
@@ -37,21 +74,13 @@ suite(void) {
 	tcase_add_test(testcase_admin_connect, test_admin_connect_normal);
 	suite_add_tcase(s, testcase_admin_connect);
 
-	// TCase *testcase_userlen_invalid = tcase_create("auth_userlen_invalid");
-	// tcase_add_test(testcase_userlen_invalid, test_auth_userlen_invalid);
-	// suite_add_tcase(s, testcase_userlen_invalid);
+	TCase* testcase_admin_connect_unsupported_version = tcase_create("admin_connect_unsupported_version");
+	tcase_add_test(testcase_admin_connect_unsupported_version, test_admin_connect_unsupported_version);
+	suite_add_tcase(s, testcase_admin_connect_unsupported_version);
 
-  // TCase *testcase_passlen_invalid = tcase_create("auth_passlen_invalid");
-	// tcase_add_test(testcase_passlen_invalid, test_auth_passlen_invalid);
-	// suite_add_tcase(s, testcase_passlen_invalid);
-
-	// TCase *testcase_unsupported_version = tcase_create("auth_unsupported_version");
-	// tcase_add_test(testcase_unsupported_version, test_auth_unsupported_version);
-	// suite_add_tcase(s, testcase_unsupported_version);
-
-	// TCase *testcase_multiple_requests = tcase_create("auth_multiple_requests");
-	// tcase_add_test(testcase_multiple_requests, test_auth_multiple_requests);
-	// suite_add_tcase(s, testcase_multiple_requests);
+  TCase* testcase_admin_connect_passlen_zero = tcase_create("admin_connect_passlen_zero");
+	tcase_add_test(testcase_admin_connect_passlen_zero, test_admin_connect_passlen_zero);
+	suite_add_tcase(s, testcase_admin_connect_passlen_zero);
 
 	return s;
 }
